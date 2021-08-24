9473f51033bb93eebb2ecceff64a48fa4ed6c3a3
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-24 15:35:13 -0300

    sockets funcionando. agora e tacalhepau

--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -1,40 +1,74 @@
 import React, { useState } from 'react';
 import './Login.css';
 
+import socket from '../../utills/io';
+
 function Login() {
   const [user, setUser] = useState('');
   const [password, setPassword] = useState('');
+  const [isDisabled, trueOrFalse] = useState(true);
+  const prefix = 'common_login__';
+  const passMin = 5;
+  socket.on('FromAPI', (data) => {
+    console.log(data);
+  });
+
+  const verifyDisabled = () => {
+    const re = /(.+)@(.+){2,}\.(.+){2,}/;
+    if (password.length >= passMin && re.test(user)) {
+      trueOrFalse(false);
+    } else {
+      trueOrFalse(true);
+    }
+  };
 
   const userChange = ({ target }) => {
     setUser(target.value);
+    verifyDisabled();
   };
+
   const passwordChange = ({ target }) => {
     setPassword(target.value);
+    verifyDisabled();
   };
+
   const login = () => {
-    console.log(`Login sucessful to ${user} with ${password}`);
+    socket.emit('login', { user, password });
   };
+
   return (
     <div className="login-Page">
       <input
         className="user-input"
         name="user"
         value={ user }
+        data-testid={ `${prefix}input-email` }
         onChange={ userChange }
         placeholder="User Name"
       />
       <input
         className="password-input"
         name="password"
+        data-testid={ `${prefix}input-password` }
         value={ password }
         onChange={ passwordChange }
         placeholder="Password"
       />
       <div className="buttons">
-        <button type="button" className="signin-button" onClick={ login }>
+        <button
+          type="button"
+          className="signin-button"
+          onClick={ login }
+          data-testid={ `${prefix}button-login` }
+          disabled={ isDisabled }
+        >
           LOGIN
         </button>
-        <button type="button" className="signup-button">
+        <button
+          type="button"
+          className="signup-button"
+          data-testid={ `${prefix}button-register` }
+        >
           Ainda não tenho conta
         </button>
       </div>
--- front-end/src/pages/Routes.js
+++ front-end/src/pages/Routes.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import Login from './Login/Login';
 
 function Routes() {
@@ -7,8 +12,7 @@ function Routes() {
     <Router>
       <Switch>
         <Route path="/login" component={ Login } />
-        <Route exact path="/" component={ Login } />
-        {/* <Route Component={NotFound} /> */}
+        <Redirect from="/" to="/login" />
       </Switch>
     </Router>
   );
